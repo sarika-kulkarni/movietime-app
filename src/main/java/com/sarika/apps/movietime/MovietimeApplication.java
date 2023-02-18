ab136610dbbd3194d8eb4b1f4edf38537b11c8c9
sarika-kulkarni <sarika.kulkarni05@gmail.com>
2023-02-18 14:47:24 -0500

    Initial project setup

---
+++ src/main/java/com/sarika/apps/movietime/MovietimeApplication.java
@@ -0,0 +1,13 @@
+package com.sarika.apps.movietime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovietimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarika/apps/movietime/MovietimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarika.apps.movietime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovietimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
