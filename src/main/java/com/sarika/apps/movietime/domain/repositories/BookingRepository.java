ff7af0396f8b96cf8864882cb7dc7676fae17fe2
sarika-kulkarni <sarika.kulkarni05@gmail.com>
2023-03-11 09:34:05 -0500

    fixed booking-api date issue

--- src/main/java/com/sarika/apps/movietime/domain/repositories/BookingRepository.java
+++ src/main/java/com/sarika/apps/movietime/domain/repositories/BookingRepository.java
@@ -4,12 +4,13 @@ import com.sarika.apps.movietime.domain.entities.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
     @Override
     <S extends Booking> S saveAndFlush(S entity);
 
-    @Query(value = "from Booking b where b.movieShow.movieShowId = ?1")
-    List<Booking> findByMovieShow(Integer movieShowId);
+    @Query(value = "from Booking b where b.movieShow.movieShowId = ?1 and b.bookingDate = ?2")
+    List<Booking> findByMovieShow(Integer movieShowId, Date bookingDate);
 }
